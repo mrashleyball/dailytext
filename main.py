237c41abc7cfa0cb7e991176beb3bdd501447af2
mrashleyball <me@mrashleyball.com>
2022-05-06 07:44:46 +1000

    tidy.up

--- main.py
+++ main.py
@@ -1,63 +1,49 @@
 #! python3
-
-##############################
 #
 # DAILY TEXT
-# 
-##############################
-
-# To Do
-# 1 Proof of concept
-# 2 Get working with WOL
-# 3 Make Discord Bot
-
-##############################
-# START OF PROGRAM
+# Author: Ashley Ball
+# Repo: https://github.com/mrashleyball/dailytext
 
 import requests, datetime
 from bs4 import BeautifulSoup
 
-def main():
-    
-    # Get's current date
-    current_date = datetime.datetime.now()
-    year = current_date.year
-    month = current_date.month
-    day = current_date.day
+# Get's current date
+current_date = datetime.datetime.now()
+year = current_date.year
+month = current_date.month
+day = current_date.day
 
-    # Passes current date to make daily URL
-    URL = (f'https://wol.jw.org/en/wol/h/r1/lp-e/{year}/{month}/{day})') # URL = (f'https://wol.jw.org/en/wol/h/r1/lp-e/2022/5/2)')
-    
-    # Passes URL to page
-    page = requests.get(URL)
+# Passes current date to make daily URL
+URL = (f'https://wol.jw.org/en/wol/h/r1/lp-e/{year}/{month}/{day})') # URL = (f'https://wol.jw.org/en/wol/h/r1/lp-e/2022/5/2)')
 
-    # Grabs page content and passes to soup
-    soup = BeautifulSoup(page.content, 'html.parser')
+# Passes URL to page
+page = requests.get(URL)
 
-    # Searches soup for the Daily Text
-    results = soup.find(id='dailyText') # elements = results.find_all('div', id='dailyText')
-    
-    # Match days date with daily text
-    if month < 10:
-        month = '0'+str(month)
-    if day < 10:
-        day = '0'+str(day)
-    days_results = results.find('div', attrs={'data-date': f'{year}-{month}-{day}T00:00:00.000Z'})
-    
-    days_date = days_results.header.h2.get_text()
-    days_quote = days_results.p.em.get_text()
-    days_scripture = days_results.p.a.em.get_text()
-    days_paragraph = days_results.find('div', class_='bodyTxt').get_text().strip()
+# Grabs page content and passes to soup
+soup = BeautifulSoup(page.content, 'html.parser')
 
-    # Display daily text
-    print(
+# Searches soup for the Daily Text
+results = soup.find(id='dailyText') # elements = results.find_all('div', id='dailyText')
+
+# Match days date with daily text
+if month < 10:
+    month = '0'+str(month)
+if day < 10:
+    day = '0'+str(day)
+days_results = results.find('div', attrs={'data-date': f'{year}-{month}-{day}T00:00:00.000Z'})
+
+days_date = days_results.header.h2.get_text()
+days_quote = days_results.p.em.get_text()
+days_scripture = days_results.p.a.em.get_text()
+days_paragraph = days_results.find('div', class_='bodyTxt').get_text().strip()
+
+# Display daily text
+print(
 f'''{days_date}
 
 {days_quote}{days_scripture}
-        
+    
 {days_paragraph}
-        
+    
 URL: {URL}''')
-    # print(days_results)
-
-main()
+# print(days_results)
