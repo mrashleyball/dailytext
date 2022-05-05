8b3310ca58ca05c664a75c180a500ff6c77b2e90
Ashley Ball <me@ashleyball.com.au>
2022-05-05 23:46:28 +0000

    Changed command '!'

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ async def on_ready():
 async def on_message(message):
   if message.author == client.user:
     return
-  if message.content.startswith('$dailytext'):
+  if message.content.startswith('!dailytext'):
     the_days_text = dailytext()
     await message.channel.send(the_days_text)
 
